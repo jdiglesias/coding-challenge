3864e29060310fc9b4fa6bde9f18c235b4fa98d0
Juan Iglesias <juandiglesias@gmail.com>
2021-08-26 19:54:44 -0700

    add balanced binary tree based solution to the problem

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,48 @@
 "use strict";
+const bst = require("../lib/bst");
 
 // Print all entries, across all of the *async* sources, in chronological order.
+//
+// This functions similarly to the synchronous implementation except that initially requests logs from all the log sources at once using Promise.all()
+// and then awaits popAsync thereafter
+//
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
+    const tree = {};
+    Promise.all(logSources.map((src, i) => Promise.all([src.popAsync(), Promise.resolve(i)])))
+      .then(async (results) =>  { 
+        results.map(([log, index]) => {
+          const node = {
+            val: log,
+            left: null,
+            right: null,
+            index: index,
+          };
+          bst.insert(tree, node);
+        });
+        let logNode;
+        do{
+          logNode = bst.pop(tree);
+          if(logNode){
+            printer.print(logNode.val);
+            const next = await logSources[logNode.index].popAsync();
+            const nextNode = {
+              val: next,
+              left: null,
+              right: null,
+              index: logNode.index
+            };
+
+            if(next){
+              bst.insert(tree, nextNode);
+            }
+          }
+        }while(logNode);
+
+      });
+    
+    printer.done();
     resolve(console.log("Async sort complete."));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,43 @@
 "use strict";
-
+const bst = require("../lib/bst");
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  // initialize the tree with one log from each source. 
+  // the earliest log is guaranteed to be one of these
+  const tree = {};
+  for(let i = 0; i<logSources.length; i++){
+    const node = {
+      val: logSources[i].last,
+      left: null,
+      right: null,
+      index: i,
+    };
+    bst.insert(tree, node);
+  }
+  let logNode;
+  // now display the earliest log and insert the next earliest log from the same source. 
+  // as long as we have the earliest remaining log from each source in the tree at all times, popping from the tree gives us the next log to print
+  do{
+    logNode = bst.pop(tree);
+    if(logNode){
+      printer.print(logNode.val);
+      const next = logSources[logNode.index].pop();
+      const nextNode = {
+        val: next,
+        left: null,
+        right: null,
+        index: logNode.index
+      };
+
+      if(next){
+        bst.insert(tree, nextNode);
+      }
+    }
+  }while(logNode);
+
+  printer.done();
+  
+    
   return console.log("Sync sort complete.");
 };
